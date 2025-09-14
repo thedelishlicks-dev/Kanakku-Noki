5f692304b8be319f6fb3eb59ca013d76b241bdbe
Razook K S <thedelishlicks@gmail.com>
2025-09-14 10:22:24 +0000

    Try fixing this error: `Build Error: Export auth doesn't exist in target

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,16 +2,16 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "test-api-key",
-  authDomain: "test-auth-domain",
-  projectId: "test-project-id",
-  storageBucket: "test-storage-bucket",
-  messagingSenderId: "test-messaging-sender-id",
-  appId: "test-app-id",
+  apiKey: "AIzaSyC_UHYdr94bhW7nNLT3rFNzyuZFG1Pyofg",
+  authDomain: "family-finance-97bcf.firebaseapp.com",
+  projectId: "family-finance-97bcf",
+  storageBucket: "family-finance-97bcf.firebasestorage.app",
+  messagingSenderId: "596902443557",
+  appId: "1:596902443557:web:7f6fb0f556e50409e25dcc"
 };
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 
-export { auth };
+export { app, auth };

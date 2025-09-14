b49d4f8c0ea89febb27ef5c3ef629c45784acfc6
Razook K S <thedelishlicks@gmail.com>
2025-09-14 10:26:38 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  apiKey: "AIzaSyDA3vmqGBPIOR9SeSXb9HMy8j2ZEaLNjpQ",
+  authDomain: "studio-2264868096-7e706.firebaseapp.com",
+  projectId: "studio-2264868096-7e706",
+  storageBucket: "studio-2264868096-7e706.firebasestorage.app",
+  messagingSenderId: "240096414245",
+  appId: "1:240096414245:web:b694dcf20755b8eed00dcc"
 };
 
 // Initialize Firebase
